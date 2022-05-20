8350c2a9620b586c4192c99ad49be6effe7883ca
sofia <sofiavign@gmail.com>
2022-05-20 00:30:14 -0300

    type

--- models/product.ts
+++ models/product.ts
@@ -1,6 +1,21 @@
 import { productsIndex } from "lib/algolia";
 import { base } from "lib/airtable";
 
+type ProductData = {
+  objectID: string;
+  name: string;
+  image: any;
+  price: number;
+  category: string;
+  description: string;
+  in_stock: boolean;
+};
+
+type resultQuery = {
+  results: ProductData[];
+  pagination: { offset: number; limit: number; total: number };
+};
+
 /**
  * @param limit number
  * @param offset number
@@ -12,14 +27,15 @@ export async function getProductsByQuery(
   limit: number,
   offset: number,
   query: string
-): Promise<Object> {
+): Promise<resultQuery> {
   const records = await productsIndex.search(query, {
     page: offset > 1 ? Math.floor(offset / limit) : 0,
     hitsPerPage: limit,
   });
 
+  const products = records.hits as any;
   return {
-    results: records.hits,
+    results: products.filter((p) => p.in_stock),
     pagination: {
       offset,
       limit,
@@ -52,7 +68,7 @@ export async function getAllProducts(): Promise<any> {
 }
 
 /**
- * @description Sync products from Airtable with Algolia every 30 minutes
+ * @description Sync products from Airtable with Algolia every 30 minutes (cron job)
  */
 export const syncAirtableWithAlgolia = async (): Promise<any> => {
   return base("Funkos")
