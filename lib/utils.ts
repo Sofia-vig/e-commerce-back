eb33947a505a5a727f34f838cf7e01d065d08d7d
sofa <sofiavign@gmail.com>
2022-07-20 04:17:51 -0300

    new code generatore

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,7 @@
-import gen from "random-seed";
 import addMinutes from "date-fns/addMinutes";
-var seed = "breakingBad";
-var random = gen.create(seed);
 
 export const generateCodeWithExpires = (): { code: number; expires: Date } => {
-  const code = random.intBetween(10000, 99999);
+  const code = Math.floor(Math.random() * 90000) + 10000;
   const now = new Date();
   const fiveMinutesFromNow = addMinutes(now, 5);
   return { code, expires: fiveMinutesFromNow };
